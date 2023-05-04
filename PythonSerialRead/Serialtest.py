498bce4a8d60a341be63e5b1f75798534fee653d
Tyrtus <60611181+Tyrtus@users.noreply.github.com>
2023-05-04 10:41:51 +0200

    Update af Python fil
    This is just a minor update, so it will print a random number

--- PythonSerialRead/Serialtest.py
+++ PythonSerialRead/Serialtest.py
@@ -1,11 +1,14 @@
 import serial
+import random 
+from random import randint
+def pulse():
+     return randint(50,90)         
 def write_to_file():
-     with open("testfile.txt", "w") as fp:
-          fp.writelines(cc[2:][:-5]+"\n" )
+     with open("PythonSerialRead/testfile.txt", "w") as fp:
+          fp.writelines(cc[2:][:-5]+"Pulse:"+str(pulse()))
           fp.close()
-          return 0
+          return print(cc[2:][:-5]+"Pulse:"+str(pulse()))
 ser = serial.Serial("COM6", 115200) #COM Port for reading 
 while True:
      cc=str(ser.readline())
-     print(cc[2:][:-5])
      write_to_file()
