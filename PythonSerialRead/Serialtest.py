24b41499aa9ecabb561a7bc8eb02cc1737cff1f5
Tyrtus <60611181+Tyrtus@users.noreply.github.com>
2023-05-11 10:53:34 +0200

    Removed a space

--- PythonSerialRead/Serialtest.py
+++ PythonSerialRead/Serialtest.py
@@ -5,7 +5,7 @@ def pulse():
      return randint(50,90)         
 def write_to_file():
      with open("testfile.txt", "w") as fp:
-          fp.writelines(cc[2:][:-5]+"Pulse: "+str(pulse()))
+          fp.writelines(cc[2:][:-5]+", "+str(pulse()))
           fp.close()
           return print(cc[2:][:-5]+", "+str(pulse()))
 ser = serial.Serial("COM6", 115200) #COM Port for reading 
