d95a905cca1e8f1c03006a6ca71e395064a7f0b7
Tyrtus <60611181+Tyrtus@users.noreply.github.com>
2023-05-16 18:12:42 +0200

    Big change top Python Script, kinda works

--- PythonSerialRead/Serialtest.py
+++ PythonSerialRead/Serialtest.py
@@ -4,10 +4,17 @@ from random import randint
 def pulse():
      return randint(50,90)         
 def write_to_file():
-     with open("PythonSerialRead/testfile.txt", "w") as fp:
-          fp.writelines(cc[2:][:-5]+"Pulse: "+str(pulse()))
-          fp.close()
-          return print(cc[2:][:-5]+", "+str(pulse()))
+     with open("testfile.txt", "w") as fp:
+          if  "S" in cc[2]: 
+               fp.write(cc[2:][:-5]) #cc[2:][:-5]
+               print(cc[2])
+               fp.close()
+               return print(cc[2:][:-5])
+          else:
+               fp.write(cc[2:][:-5]+str(pulse()))
+               print(cc[2])
+               fp.close()
+               return print(cc[2:][:-5])
 ser = serial.Serial("COM6", 115200) #COM Port for reading 
 while True:
      cc=str(ser.readline())
